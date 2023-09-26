00cae1fdea830907686b11aa9d7e9c33be1cb82e
kushalnigam0604 <nigamkushal0@gmail.com>
2023-09-26 13:37:43 +0530

    Add code

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 function Footer() {
-    const buttons = ['evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin'];
+    const values = ['evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin', 'evolphin'];
     return (
         <div className='text-white flex flex-col items-center'>
             <div className='m-3 text-gray-300'>
                 Leading the charge for industries!
             </div>
-            <div className="flex justify-around w-full">
-                {buttons.map((label, index) => (
-                    <button key={index} className="text-sm text-black bg-white py-1 px-5 rounded-md">
-                        {label}
-                    </button>
+            <ul className="flex items-center list-none max-w-full overflow-x-auto overflow-y-hidden scroll-smooth scrollbar-none no-scrollbar">
+                {values.map((d, i) => (
+                    <li className="bg-gray-500 text-white p-2 rounded-md mx-2 whitespace-nowrap">
+                        {d}
+                    </li>
                 ))}
-            </div>
+
+            </ul>
+
         </div>
     )
 }
